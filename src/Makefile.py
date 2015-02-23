ca109becb2602775acb2255569ee3ca92d0ed862
Dmitry Sovetov <DSovetov@dm.local>
2015-02-23 18:12:17 +0200

    Added math module

--- src/Makefile.py
+++ src/Makefile.py
@@ -1,4 +1,6 @@
-Project( externals = '../externals' )
+Project( externals = '../externals', paths = '../modules' )
 
 Include( 'demo' )
 Include( 'relight' )
+
+Module( '../modules', 'https://github.com/dmsovetov/math.git' )
